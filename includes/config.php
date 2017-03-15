56eba8de20d86c41351362ed1eb9e808b6b5b839
yakuter <yakuter@gmail.com>
2017-03-15 15:48:39 +0000

    update

--- includes/config.php
+++ includes/config.php
@@ -2,10 +2,10 @@
 	
 	//==================yPhoneBook Configuration=======================//
 
-	$site_url		=	"http://myworks-yakuter.c9users.io/yPhoneBook";		// <-- website url without "/"
-	$limit			=	10;						    // <-- contact number for each page (for further versions)
-	$db_user		=	"yakuter";				// <-- database user name
-	$db_password	=	"";						  // <-- database user password
+	$site_url		=	"";						// <-- website url without "/"
+	$limit			=	10;						// <-- contact number for each page (for further versions)
+	$db_user		=	"";						// <-- database user name
+	$db_password	=	"";						// <-- database user password
 	$db_name		=	"yPhoneBook";			// <-- database name
 	$db_table		=	"contacts";				// <-- database table
 	$db_host		=	"localhost";			// <-- database host
